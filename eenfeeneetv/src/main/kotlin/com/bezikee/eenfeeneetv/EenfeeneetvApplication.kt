2e4cfdb4a736c89358b6fb7450aaa147f3c80584
Andres Rubio <andresfra92@gmail.com>
2022-06-16 16:13:04 -0400

    Initial commit

---
+++ eenfeeneetv/src/main/kotlin/com/bezikee/eenfeeneetv/EenfeeneetvApplication.kt
@@ -0,0 +1,11 @@
+package com.bezikee.eenfeeneetv
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EenfeeneetvApplication
+
+fun main(args: Array<String>) {
+	runApplication<EenfeeneetvApplication>(*args)
+}
---
+++ eenfeeneetv/src/test/kotlin/com/bezikee/eenfeeneetv/EenfeeneetvApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bezikee.eenfeeneetv
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EenfeeneetvApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
